71c0fca43df4fa5c1305c9fcbe94108805288ad4
unknown <ruan_03@hotmail.com>
2015-11-28 15:07:06 -0200

    Classe Batida Add

---
+++ app/src/main/java/domain/Batida.java
@@ -0,0 +1,14 @@
+package domain;
+
+/**
+ * Created by Ruan on 28/11/2015.
+ */
+public class Batida {
+    private int idBatida;
+    private Cronometragem cronometragem;
+    private int minutos;
+    private int segundos;
+    private int centezimos;
+    private boolean utilizar;
+
+}
