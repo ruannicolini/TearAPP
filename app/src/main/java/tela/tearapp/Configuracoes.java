2171892b889c29310bb9cc1cce81856435f33b0c
unknown <ruan_03@hotmail.com>
2015-11-29 23:07:46 -0200

    Implementando Tela de Configuracoes

--- app/src/main/java/tela/tearapp/Configuracoes.java
+++ app/src/main/java/tela/tearapp/Configuracoes.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Configuracoes extends Activity {
 
@@ -33,5 +34,13 @@ public class Configuracoes extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void cancelar(View view){
+        finish();
+    }
+    
+    public void salvar(View view){
+
     }
 }
